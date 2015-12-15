52f62b84c5ff17cca23eb694a958c7a6b13ab4a2
Sangho <sh31659@gmail.com>
2015-12-15 16:10:57 +0900

    Update LoadingActivity.java
    Changed which UI pops up first when starting the app.

--- eyEscort_guildance/EyEscort_guidance/app/src/main/java/com/example/gunhee/eyescort_guidance/LoadingActivity.java
+++ eyEscort_guildance/EyEscort_guidance/app/src/main/java/com/example/gunhee/eyescort_guidance/LoadingActivity.java
@@ -5,21 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.parse.Parse;
+
 /**
  * Created by gunhee on 2015-11-13.
  */
+
+
 public class LoadingActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+//        // Parse
+//        Parse.initialize(this, "9DEuwzU9jxQRuA7FghaslDVT72WysCYvLRrm1Cxo", "LWYAzhDfKf11TrgVwLIup4tkg1cgbPo5uIjqcvMT");
+
         Handler hd = new Handler();
         hd.postDelayed(new splashhandler() , 2500); // 3초 후에 hd Handler 실행
     }
 
     private class splashhandler implements Runnable{
         public void run() {
-            startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩이 끝난후 이동할 Activity
+            startActivity(new Intent(getApplication(), FinalActivity.class)); // 로딩이 끝난후 이동할 Activity
             LoadingActivity.this.finish(); // 로딩페이지 Activity Stack에서 제거
         }
     }
